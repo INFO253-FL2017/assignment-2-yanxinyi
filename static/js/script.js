50ce032bc3ff58afd47059766153a6dd07b0d6ae
yanxinyi <yanxinyi@gmail.com>
2017-10-15 16:11:04 -0700

    finished

--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,26 @@ contactMeButton.addEventListener("click", function(event) {
     var name = document.getElementById("contact_me_name_id").value;
     var subject = document.getElementById("contact_me_subject_id").value;
     var msg = document.getElementById("contact_me_msg_id").value;
+    var email = document.getElementById("contact_me_email_id").value;
 
-    if (name.length == 0 || subject.length == 0 || msg.length == 0) {
+
+
+    if (name.length == 0 || subject.length == 0 || msg.length == 0 || email.length == 0) {
         var notice = "You should fill";
         if (name.length == 0) {
             notice += " name";
         }
+
         if (subject.length == 0) {
             notice += " subject";
         }
 
         if (msg.length == 0) {
             notice += " message";
+        }
+
+        if (email.length == 0) {
+            notice += " email";
         }
 
         document.getElementById("contact_me_notice").style.display = "block";
@@ -25,7 +33,7 @@ contactMeButton.addEventListener("click", function(event) {
         var http_request = new XMLHttpRequest();
 		http_request.open("POST","/contactMeFormPost",true);
 
-		http_request.send('{"name":"' + name  + '","email":"' +  "yanxinyi@berkeley.edu" + '","subject":"' + subject + '","msg":"' + msg + '"}')
+		http_request.send('{"name":"' + name  + '","email":"' +  email + '","subject":"' + subject + '","msg":"' + msg + '"}')
 
         document.getElementById("contact_me_notice").style.display = "block";
         document.getElementById("contact_me_notice").innerHTML = "Hi " +  name + ", your message has been sent";
